9493822693f6db47e9b2f85fff6f1ff534160432
Nicolas Sauzede <nsauzede@laposte.net>
2019-09-16 02:26:09 +0200

    Accomodate V array repeat syntax change

--- tetrisnomix_v.v
+++ tetrisnomix_v.v
@@ -282,7 +282,7 @@ fn (g mut Game) init_game() {
 	g.field = []array_int // TODO: g.field = [][]int
 	// Generate the field, fill it with 0's, add -1's on each edge
 	for i := 0; i < FieldHeight + 2; i++ {
-		mut row := [0; FieldWidth + 2]
+		mut row := [0].repeat(FieldWidth + 2)
 		row[0] = - 1
 		row[FieldWidth + 1] = - 1
 		g.field << row
@@ -491,7 +491,7 @@ fn (g &Game) draw_score() {
 
 fn parse_binary_tetro(t_ int) []Block {
 	mut t := t_
-	res := [Block{} ; 4]
+	res := [Block{}].repeat(4)
 	mut cnt := 0
 	horizontal := t == 9// special case for the horizontal line
 	for i := 0; i <= 3; i++ {
