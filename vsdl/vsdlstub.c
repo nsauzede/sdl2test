69063c515a7c4b8790e676764740d755c3204688
Nicolas Sauzede <nsauzede@laposte.net>
2019-09-16 02:45:12 +0200

    Remove obsolete sdl stubs

--- vsdl/vsdlstub.c
+++ vsdl/vsdlstub.c
@@ -1,7 +1,7 @@
 #include <inttypes.h>
 #include <stdio.h>
 
-#include <SDL_ttf.h>
+#define dbgprintf(...) do{}while(0)
 
 /*
         This dumb C STUB is to work around the fact V transpiler fails
@@ -9,7 +9,13 @@
         and also fails to allow returning a 64 bits void pointer.
 */
 
-#define dbgprintf(...) do{}while(0)
+// obsolete
+// use this V instead :
+// text string, tcol SdlColor
+// tsurf := C.TTF_RenderText_Solid(g.font, text.str, tcol)
+#if 0
+
+#include <SDL_ttf.h>
 
 DECLSPEC void SDLCALL stubTTF_RenderText_Solid(TTF_Font *font,
                 const char *text, SDL_Color *fg, SDL_Surface **ret) {
@@ -24,10 +30,25 @@ DECLSPEC void SDLCALL stubTTF_RenderText_Solid(TTF_Font *font,
         *ret = res;
 }
 
+#endif
+
+// obsolete -- use this V myrand_r until V rand_r is available
+// Ported from https://git.musl-libc.org/cgit/musl/diff/src/prng/rand_r.c?id=0b44a0315b47dd8eced9f3b7f31580cf14bbfc01
+// Thanks spytheman
+//fn myrand_r(seed &int) int {
+//  mut rs := seed
+//  ns := ( *rs * 1103515245 + 12345 )
+//  *rs = ns
+//  return ns & 0x7fffffff
+//}
+#if 0
+
 #ifdef WIN32
 int rand_r(unsigned int *seedp) {
 	srand(*seedp);
 	*seedp = rand();
 	return *seedp;
 }
+#endif
+
 #endif
