ab53332f4f07cfc688b5291806e48dc0f0b5a23f
Nicolas Sauzede <nsauzede@laposte.net>
2019-10-04 22:33:56 +0200

    Add wireframe key

--- glfnt.cpp
+++ glfnt.cpp
@@ -146,7 +146,7 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         RenderText(shader, "This is sample text", 25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
-        RenderText(shader, "(C) LearnOpenGL.com", 540.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
+        RenderText(shader, "(C) LearnOpenGL.com", 520.0f, 570.0f, 0.5f, glm::vec3(0.3, 0.7f, 0.9f));
 
         // Swap the buffers
         glfwSwapBuffers(window);
