9446ebb3db8ab4a2233853ddeba7f480f502226a
Nicolas Sauzede <nsauzede@laposte.net>
2020-10-05 03:24:57 +0200

    Improve cratesan Rust to draw graphical screen

--- cratesan/Rust/src/main.rs
+++ cratesan/Rust/src/main.rs
@@ -2,6 +2,10 @@ extern crate sdl2;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::{Canvas, Texture, TextureCreator};
+use sdl2::video::{Window, WindowContext};
 
 use std::env::current_exe;
 use std::fs::File;
@@ -12,10 +16,20 @@ use std::time::Duration;
 const EMPTY: u8 = 0x0;
 const STORE: u8 = 0x1;
 const CRATE: u8 = 0x2;
-const PLAYER: u8 = 0x4;
+//const PLAYER: u8 = 0x4;
 const WALL: u8 = 0x8;
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 200;
+const WIDTH: usize = 320;
+const HEIGHT: usize = 200;
+const NUM_TEXTURES: usize = 9;
+const TEX_PLAYER: usize = 0;
+const TEX_EMPTY: usize = 1;
+const TEX_SPLAYER: usize = 2;
+const TEX_STORE: usize = 3;
+const TEX_CRATE: usize = 4;
+const TEX_WALL: usize = 5;
+const TEX_STOREDW: usize = 6;
+const TEX_STORED: usize = 7;
+const TEX_INV: usize = 8;
 const C_EMPTY: char = ' ';
 const C_STORE: char = '.';
 const C_STORED: char = '*';
@@ -35,19 +49,32 @@ struct Level {
     px: usize,
     py: usize,
 }
+
+enum Status {
+    Play,
+    Win,
+}
+
 struct Game {
     quit: bool,
-    win: bool,
+    status: Status,
     must_draw: bool,
     levels: Vec<Level>,
     level: usize,
     // following are copies of current level's
     crates: u32,
     stored: u32,
+    /// map dims
     w: usize,
     h: usize,
+    /// player pos
     px: usize,
     py: usize,
+    /// block dims
+    bw: usize,
+    bh: usize,
+    width: usize,
+    height: usize,
 }
 
 impl Game {
@@ -89,7 +116,7 @@ impl Game {
         for s in vlevels {
             let mut map = Vec::new();
             let mut crates = 0;
-            let mut storages = 0;
+            let mut stores = 0;
             let mut stored = 0;
             let mut w = 0;
             let mut h = 0;
@@ -113,7 +140,7 @@ impl Game {
                         }
                         C_STORE => {
                             v[i] = STORE;
-                            storages += 1;
+                            stores += 1;
                         }
                         C_CRATE => {
                             v[i] = CRATE;
@@ -121,7 +148,7 @@ impl Game {
                         }
                         C_STORED => {
                             v[i] = CRATE | STORE;
-                            storages += 1;
+                            stores += 1;
                             crates += 1;
                             stored += 1;
                         }
@@ -142,7 +169,7 @@ impl Game {
                             py = h;
                             player_found = true;
                             v[i] = STORE;
-                            storages += 1;
+                            stores += 1;
                         }
                         C_WALL => {
                             v[i] = WALL;
@@ -155,10 +182,10 @@ impl Game {
                 map.push(v);
                 h += 1;
             }
-            if crates != storages {
+            if crates != stores {
                 panic!(
-                    "Mismatch between crates={} and storages={} in level",
-                    crates, storages
+                    "Mismatch between crates={} and stores={} in level",
+                    crates, stores
                 );
             }
             if !player_found {
@@ -178,11 +205,11 @@ impl Game {
         }
         levels
     }
-    fn new() -> Game {
+    fn new(width: usize, height: usize) -> Game {
         let levels = Game::load_levels();
         let mut g = Game {
             quit: false,
-            win: false,
+            status: Status::Play,
             must_draw: true,
             levels,
             level: 0,
@@ -192,13 +219,17 @@ impl Game {
             h: 0,
             px: 0,
             py: 0,
+            bw: 0,
+            bh: 0,
+            width,
+            height,
         };
         g.set_level(0);
         g
     }
     fn set_level(&mut self, level: usize) -> bool {
         if level < self.levels.len() {
-            self.win = false;
+            self.status = Status::Play;
             self.must_draw = true;
             self.level = level;
             self.crates = self.levels[level].crates;
@@ -207,6 +238,8 @@ impl Game {
             self.h = self.levels[level].h;
             self.px = self.levels[level].px;
             self.py = self.levels[level].py;
+            self.bw = self.width / self.w;
+            self.bh = self.height / self.h;
             true
         } else {
             false
@@ -246,7 +279,8 @@ impl Game {
                 if self.levels[self.level].map[to_y][to_x] & STORE == STORE {
                     self.stored += 1;
                     if self.stored == self.crates {
-                        self.win = true;
+                        self.status = Status::Win;
+                        println!("YOU WIN level {} !!!!", self.level + 1);
                     }
                 }
                 do_it = true;
@@ -260,121 +294,206 @@ impl Game {
             self.must_draw = true;
         }
     }
-    fn draw_map(&mut self) {
+    fn draw_map(
+        &mut self,
+        canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
+        textures: &[sdl2::render::Texture<'_>; NUM_TEXTURES],
+    ) {
         if self.must_draw {
+            canvas.set_draw_color(Color::RGB(0, 0, 0));
+            canvas.clear();
+            let x = (WIDTH - self.w * self.bw) / 2;
+            let y = (HEIGHT - self.h * self.bh) / 2;
             for (j, line) in self.levels[self.level].map.iter().enumerate() {
                 for (i, &e) in line.iter().enumerate() {
-                    let c = if e == EMPTY {
+                    let idx = if e == EMPTY {
                         if self.px == i && self.py == j {
-                            C_PLAYER
+                            TEX_PLAYER
                         } else {
-                            C_EMPTY
+                            TEX_EMPTY
                         }
                     } else if e == STORE {
                         if self.px == i && self.py == j {
-                            C_SPLAYER
+                            TEX_SPLAYER
                         } else {
-                            C_STORE
+                            TEX_STORE
                         }
                     } else if e == CRATE {
-                        C_CRATE
+                        TEX_CRATE
                     } else if e == WALL {
-                        C_WALL
-                    } else if e == PLAYER {
-                        C_PLAYER
+                        TEX_WALL
                     } else if e == CRATE | STORE {
-                        C_STORED
-                    } else if e == PLAYER | STORE {
-                        C_SPLAYER
+                        match self.status {
+                            Status::Win => TEX_STOREDW,
+                            _ => TEX_STORED,
+                        }
                     } else {
-                        '?'
+                        TEX_INV
                     };
-                    print!("{}", c);
+                    canvas
+                        .copy(
+                            &textures[idx as usize],
+                            None,
+                            Rect::new(
+                                (x + i * self.bw) as i32,
+                                (y + j * self.bh) as i32,
+                                self.bw as u32,
+                                self.bh as u32,
+                            ),
+                        )
+                        .expect("Couldn't copy texture into window");
                 }
-                println!();
             }
+            canvas.present();
             self.must_draw = false;
         }
     }
 
     fn handle_events(&mut self, event_pump: &mut sdl2::EventPump) {
-        if self.win {
-            println!("YOU WIN level {} !!!!", self.level + 1);
-            if self.set_level(self.level + 1) {
-            } else {
-                self.quit = true;
-            }
-            return;
-        }
         for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. } => {
+            if !match self.status {
+                Status::Win => self.handle_event_win(event),
+                Status::Play => self.handle_event_play(event),
+            } {
+                break;
+            }
+        }
+    }
+    fn handle_event_win(&mut self, event: sdl2::event::Event) -> bool {
+        let mut cont = true;
+        match event {
+            Event::Quit { .. } => {
+                self.quit = true;
+                cont = false;
+            }
+            Event::KeyDown {
+                keycode: Some(k), ..
+            } => match k {
+                Keycode::Escape => {
                     self.quit = true;
-                    break;
+                    cont = false;
                 }
-                Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => {
+                Keycode::Return => {
+                    if self.set_level(self.level + 1) {
+                    } else {
+                        self.quit = true;
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+        cont
+    }
+    fn handle_event_play(&mut self, event: sdl2::event::Event) -> bool {
+        let mut cont = true;
+        match event {
+            Event::Quit { .. } => {
+                self.quit = true;
+                cont = false;
+            }
+            Event::KeyDown {
+                keycode: Some(k), ..
+            } => match k {
+                Keycode::Escape => {
                     self.quit = true;
-                    break;
+                    cont = false;
                 }
-                Event::KeyDown {
-                    keycode: Some(Keycode::Down),
-                    ..
-                } => {
-                    self.try_move(0, 1);
-                }
-                Event::KeyDown {
-                    keycode: Some(Keycode::Right),
-                    ..
-                } => {
-                    self.try_move(1, 0);
-                }
-                Event::KeyDown {
-                    keycode: Some(Keycode::Left),
-                    ..
-                } => {
-                    self.try_move(-1, 0);
-                }
-                Event::KeyDown {
-                    keycode: Some(Keycode::Up),
-                    ..
-                } => {
+                Keycode::Up => {
                     self.try_move(0, -1);
                 }
-                Event::KeyDown {
-                    keycode: Some(k), ..
-                } if (k == Keycode::RCtrl
-                    || k == Keycode::LCtrl
-                    || k == Keycode::PageDown
-                    || k == Keycode::Space) => {}
+                Keycode::Down => {
+                    self.try_move(0, 1);
+                }
+                Keycode::Left => {
+                    self.try_move(-1, 0);
+                }
+                Keycode::Right => {
+                    self.try_move(1, 0);
+                }
                 _ => {}
-            }
+            },
+            _ => {}
         }
+        cont
+    }
+}
+
+fn create_texture_rect<'a>(
+    canvas: &mut Canvas<Window>,
+    texture_creator: &'a TextureCreator<WindowContext>,
+    r: u8,
+    g: u8,
+    b: u8,
+    width: u32,
+    height: u32,
+) -> Option<Texture<'a>> {
+    if let Ok(mut square_texture) = texture_creator.create_texture_target(None, width, height) {
+        canvas
+            .with_texture_canvas(&mut square_texture, |texture| {
+                texture.set_draw_color(Color::RGB(r, g, b));
+                texture.clear();
+            })
+            .expect("Failed to color a texture");
+        Some(square_texture)
+    } else {
+        None
     }
 }
 
 fn main() {
+    let title = "クレートさん";
+    let width = WIDTH;
+    let height = HEIGHT;
+    let mut game = Game::new(width, height);
     let sdl_context = sdl2::init().expect("SDL initialization failed");
     let video_subsystem = sdl_context
         .video()
         .expect("Couldn't get SDL video subsystem");
-    let width = WIDTH;
-    let height = HEIGHT;
-    let _window = video_subsystem
-        .window("クレートさん", width, height)
+    let window = video_subsystem
+        .window(title, width as u32, height as u32)
         .position_centered()
         .build()
         .expect("Failed to create window");
+    let mut canvas = window
+        .into_canvas()
+        .target_texture()
+        .present_vsync()
+        .build()
+        .expect("Couldn't get window's canvas");
     let mut event_pump = sdl_context.event_pump().expect(
         "Failed to get
           SDL event pump",
     );
-    let mut game = Game::new();
+    let texture_creator: TextureCreator<_> = canvas.texture_creator();
+    macro_rules! texture {
+        ($r:expr, $g:expr, $b:expr) => {
+            create_texture_rect(
+                &mut canvas,
+                &texture_creator,
+                $r,
+                $g,
+                $b,
+                game.bw as u32,
+                game.bh as u32,
+            )
+            .unwrap()
+        };
+    }
+    let textures = [
+        texture!(255, 255, 255),
+        texture!(66, 66, 66),
+        texture!(190, 190, 190),
+        texture!(105, 105, 105),
+        texture!(156, 100, 63),
+        texture!(255, 0, 0),
+        texture!(235, 178, 0),
+        texture!(109, 69, 43),
+        texture!(0, 255, 0),
+    ];
     while !game.quit {
         game.handle_events(&mut event_pump);
-        game.draw_map();
+        game.draw_map(&mut canvas, &textures);
         sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
