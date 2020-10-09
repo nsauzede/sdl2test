fd8d536b3891de2b4d4f73fbf5a35a88c2abf5f4
Nicolas Sauzede <nsauzede@laposte.net>
2020-10-09 17:59:57 +0200

    Add Undo, Pause, Win (cheat), fonts and other goodies for V

--- cratesan/V/cratesan.v
+++ cratesan/V/cratesan.v
@@ -3,8 +3,14 @@ import nsauzede.vsdl2.image as img
 import os
 
 const (
-	width       = 320 * 2
-	height      = 200 * 2
+	zoom        = 2
+	text_size   = 16
+	text_ratio  = zoom * 0 + 1
+	white       = vsdl2.Color{255, 255, 255, 0}
+	black       = vsdl2.Color{0, 0, 0, 0}
+	text_color  = black
+	width       = 320 * zoom
+	height      = 200 * zoom
 	empty       = 0x0
 	store       = 0x1
 	crate       = 0x2
@@ -17,14 +23,16 @@ const (
 	c_splayer   = `&`
 	c_wall      = `#`
 	bpp         = 32
-	levels_file = os.resource_abs_path('../res/levels/levels.txt')
-	i_empty     = os.resource_abs_path('../res/images/empty.png')
-	i_store     = os.resource_abs_path('../res/images/store.png')
-	i_stored    = os.resource_abs_path('../res/images/stored.png')
-	i_crate     = os.resource_abs_path('../res/images/crate.png')
-	i_player    = os.resource_abs_path('../res/images/player.png')
-	i_splayer   = os.resource_abs_path('../res/images/splayer.png')
-	i_wall      = os.resource_abs_path('../res/images/wall.png')
+	res_dir     = os.resource_abs_path('../res')
+	font_file   = res_dir + '/fonts/RobotoMono-Regular.ttf'
+	levels_file = res_dir + '/levels/levels.txt'
+	i_empty     = res_dir + '/images/empty.png'
+	i_store     = res_dir + '/images/store.png'
+	i_stored    = res_dir + '/images/stored.png'
+	i_crate     = res_dir + '/images/crate.png'
+	i_player    = res_dir + '/images/player.png'
+	i_splayer   = res_dir + '/images/splayer.png'
+	i_wall      = res_dir + '/images/wall.png'
 	n_empty     = 0
 	n_store     = 1
 	n_stored    = 2
@@ -36,6 +44,7 @@ const (
 
 enum Status {
 	play
+	pause
 	win
 }
 
@@ -68,6 +77,10 @@ mut:
 	lev         Level
 	undo_states []UndoState
 	level       int
+	moves       int
+	pushes      int
+	total_time  u64
+	last_ticks  u32
 	// following are copies of current level's
 	crates      int
 	stored      int
@@ -89,6 +102,8 @@ mut:
 	height      int
 	block_surf  []&vsdl2.Surface
 	block_text  []voidptr
+	// TTF context for font drawing
+	font        voidptr
 }
 
 fn load_levels() []Level {
@@ -209,14 +224,19 @@ fn (mut g Game) set_level(level int) bool {
 		g.level = level
 		g.lev = g.levels[level]
 		g.lev.map = g.levels[level].map.clone()
+		g.undo_states = []UndoState{}
 		g.crates = g.levels[level].crates
 		g.stored = g.levels[level].stored
 		g.w = g.levels[level].w
 		g.h = g.levels[level].h
+		g.pushes = 0
+		g.moves = 0
+		g.total_time = 0
+		g.last_ticks = vsdl2.get_ticks()
 		g.px = g.levels[level].px
 		g.py = g.levels[level].py
 		g.bw = g.width / g.w
-		g.bh = g.height / g.h
+		g.bh = (g.height - text_size * text_ratio) / g.h
 		return true
 	} else {
 		return false
@@ -245,6 +265,9 @@ fn (mut g Game) delete() {
 			vsdl2.free_surface(s)
 		}
 	}
+	if !isnil(g.font) {
+		C.TTF_CloseFont(g.font)
+	}
 }
 
 fn new_game(title string) Game {
@@ -256,15 +279,19 @@ fn new_game(title string) Game {
 		must_draw: true
 		levels: levels
 		screen: 0
+		font: 0
 	}
 	C.SDL_Init(C.SDL_INIT_VIDEO)
 	C.atexit(C.SDL_Quit)
+	C.TTF_Init()
+	C.atexit(C.TTF_Quit)
 	vsdl2.create_window_and_renderer(width, height, 0, &g.window, &g.renderer)
 	C.SDL_SetWindowTitle(g.window, g.title.str)
 	g.screen = vsdl2.create_rgb_surface(0, width, height, bpp, 0x00FF0000, 0x0000FF00,
 		0x000000FF, 0xFF000000)
 	g.texture = C.SDL_CreateTexture(g.renderer, C.SDL_PIXELFORMAT_ARGB8888, C.SDL_TEXTUREACCESS_STREAMING,
 		width, height)
+	g.font = C.TTF_OpenFont(font_file.str, text_size)
 	g.width = width
 	g.height = height
 	g.set_level(0)
@@ -308,7 +335,6 @@ fn (mut g Game) try_move(dx, dy int) {
 				g.stored++
 				if g.stored == g.crates {
 					g.status = .win
-					println('You win level ${g.level+1} ! Press RETURN to proceed..')
 				}
 			}
 			do_it = true
@@ -322,44 +348,56 @@ fn (mut g Game) try_move(dx, dy int) {
 			px: g.px
 			py: g.py
 		}
+		if map.len > 0 {
+			g.pushes++
+		}
+		g.moves++
 		g.px = x
 		g.py = y
 		g.must_draw = true
 	}
 }
 
+fn (g &Game) draw_text(x, y int, text string, tcol vsdl2.Color) {
+	if !isnil(g.font) {
+		tcol_ := C.SDL_Color{tcol.r, tcol.g, tcol.b, tcol.a}
+		tsurf := C.TTF_RenderText_Solid(g.font, text.str, tcol_)
+		ttext := C.SDL_CreateTextureFromSurface(g.renderer, tsurf)
+		texw := 0
+		texh := 0
+		C.SDL_QueryTexture(ttext, 0, 0, &texw, &texh)
+		dstrect := vsdl2.Rect{x, y, texw, texh}
+		// vsdl2.render_copy(g.sdl.renderer, ttext, 0, &dstrect)
+		C.SDL_RenderCopy(g.renderer, ttext, voidptr(0), voidptr(&dstrect))
+		C.SDL_DestroyTexture(ttext)
+		vsdl2.free_surface(tsurf)
+	}
+}
+
 fn (mut g Game) draw_map() {
+	curr_ticks := vsdl2.get_ticks()
+	if curr_ticks > g.last_ticks + 1000 {
+		if g.status == .play {
+			g.total_time += curr_ticks - g.last_ticks
+		}
+		g.last_ticks = curr_ticks
+		g.must_draw = true
+	}
 	if g.must_draw {
 		C.SDL_RenderClear(g.renderer)
 		mut rect := vsdl2.Rect{0, 0, g.width, g.height}
 		mut col := vsdl2.Color{byte(0), byte(0), byte(0), byte(255)}
 		vsdl2.fill_rect(g.screen, &rect, col)
+		// bottom status bar
+		rect = vsdl2.Rect{0, height - text_size * text_ratio, g.width, text_size * text_ratio}
+		vsdl2.fill_rect(g.screen, &rect, white)
+		C.SDL_UpdateTexture(g.texture, 0, g.screen.pixels, g.screen.pitch)
+		C.SDL_RenderCopy(g.renderer, g.texture, voidptr(0), voidptr(0))
 		x := (width - g.w * g.bw) / 2
-		y := (height - g.h * g.bh) / 2
+		y := 0
 		for j, line in g.lev.map {
 			for i, e in line {
 				rect = vsdl2.Rect{x + i * g.bw, y + j * g.bh, g.bw, g.bh}
-				col = match e {
-					empty {
-						if g.px == i && g.py == j { vsdl2.Color{byte(255), byte(255), byte(255), byte(0)} } else { vsdl2.Color{byte(66), byte(66), byte(66), byte(0)} }
-					}
-					store {
-						if g.px == i && g.py == j { vsdl2.Color{byte(190), byte(190), byte(190), byte(0)} } else { vsdl2.Color{byte(105), byte(105), byte(105), byte(0)} }
-					}
-					crate {
-						vsdl2.Color{byte(156), byte(100), byte(63), byte(0)}
-					}
-					wall {
-						vsdl2.Color{byte(255), byte(0), byte(0), byte(0)}
-					}
-					crate | store {
-						if g.status == .win { vsdl2.Color{byte(235), byte(178), byte(0), byte(0)} } else { vsdl2.Color{byte(109), byte(69), byte(43), byte(0)} }
-					}
-					else {
-						vsdl2.Color{byte(0), byte(255), byte(0), byte(0)}
-					}
-				}
-				vsdl2.fill_rect(g.screen, &rect, col)
 				tex := match e {
 					empty {
 						if g.px == i && g.py == j { g.block_text[n_player] } else { g.block_text[n_empty] }
@@ -385,10 +423,14 @@ fn (mut g Game) draw_map() {
 				}
 			}
 		}
-		if g.block_text.len == 0 {
-			C.SDL_UpdateTexture(g.texture, 0, g.screen.pixels, g.screen.pitch)
-			C.SDL_RenderCopy(g.renderer, g.texture, voidptr(0), voidptr(0))
+		time_s := g.total_time / 1000
+		state := match g.status {
+			.win { 'You win! Press Return..' }
+			.pause { '*PAUSE* Press Space..' }
+			else { '' }
 		}
+		g.draw_text(0, g.height - text_size * text_ratio - 4, '${g.level+1:02d}| moves: ${g.moves:04d} pushes: ${g.pushes:04d} time:0:00:${time_s:02} $state',
+			text_color)
 		C.SDL_RenderPresent(g.renderer)
 		g.must_draw = false
 	}
@@ -401,6 +443,7 @@ fn (mut g Game) handle_events() {
 		cont = match g.status {
 			.win { g.handle_event_win(ev) }
 			.play { g.handle_event_play(ev) }
+			.pause { g.handle_event_pause(ev) }
 		}
 	}
 }
@@ -419,15 +462,23 @@ fn (mut g Game) handle_event_play(ev vsdl2.Event) bool {
 					g.quit = true
 					cont = false
 				}
+				C.SDLK_SPACE {
+					g.status = .pause
+				}
 				C.SDLK_r {
 					g.set_level(g.level)
+				}
+				C.SDLK_w {
+					g.status = .win
 				}
 				C.SDLK_u {
 					if g.undo_states.len > 0 {
 						state := g.undo_states.pop()
 						if state.map.len > 0 {
+							g.pushes--
 							g.lev.map = state.map
 						}
+						g.moves--
 						g.px = state.px
 						g.py = state.py
 						g.must_draw = true
@@ -444,6 +495,31 @@ fn (mut g Game) handle_event_play(ev vsdl2.Event) bool {
 				}
 				C.SDLK_RIGHT {
 					g.try_move(1, 0)
+				}
+				else {}
+			}
+		}
+		else {}
+	}
+	return cont
+}
+
+fn (mut g Game) handle_event_pause(ev vsdl2.Event) bool {
+	mut cont := true
+	match int(ev.@type) {
+		C.SDL_QUIT {
+			g.quit = true
+			cont = false
+		}
+		C.SDL_KEYDOWN {
+			key := ev.key.keysym.sym
+			match key {
+				C.SDLK_ESCAPE {
+					g.quit = true
+					cont = false
+				}
+				C.SDLK_SPACE {
+					g.status = .play
 				}
 				else {}
 			}
@@ -470,6 +546,8 @@ fn (mut g Game) handle_event_win(ev vsdl2.Event) bool {
 				C.SDLK_RETURN {
 					if g.set_level(g.level + 1) {
 					} else {
+						println('Game over.')
+						g.quit = true
 						cont = false
 					}
 				}
